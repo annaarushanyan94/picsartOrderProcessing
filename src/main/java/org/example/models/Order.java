e881bcd61c2edbf9c9ff3e8139d6c497c2176483
Annaarushanyan <anna.arushanyan@picsart.com>
2025-06-05 11:47:02 +0400

    homework Customer object and array list

--- src/main/java/org/example/models/Order.java
+++ src/main/java/org/example/models/Order.java
@@ -1,31 +1,50 @@
 package org.example.models;
 
-public class Order {
+public class Order implements Comparable<Order> {
     protected Customer customer;
-    protected  double price;
-    public Order(String customName){
-        this.customer= customer;
+    protected double price;
+
+    // Վարկած՝ Default constructor (հեռացրու եթե պետք չէ)
+    public Order() {
+        this.customer = null;
         this.price = 0;
     }
+
     public Order(Customer customer, double price) {
+        if (customer == null || customer.getFirstName() == null || customer.getFirstName().isEmpty()) {
+            throw new IllegalArgumentException("Customer's first name cannot be null or empty");
+        }
         this.customer = customer;
         this.price = price;
     }
-    public void printSummary (){
-        System.out.println("Customer: " + customer + " Price: AMD" + price);
+
+    public void printSummary() {
+        System.out.println("Customer: " + customer + " | Price: AMD " + price);
     }
+
     public double getPrice() {
         return this.price;
     }
-    public void  setPrice(double price){
+
+    public void setPrice(double price) {
         this.price = price;
     }
-    public Customer getCustomer(){
+
+    public Customer getCustomer() {
         return customer;
-
-    }
-    public void setCustomer(Customer customer){
-        this.customer = customer ;
     }
 
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public String toString() {
+        return customer + " | Price: AMD " + price;
+    }
+
+    @Override
+    public int compareTo(Order o) {
+        return Double.compare(this.price, o.price);
+    }
 }
