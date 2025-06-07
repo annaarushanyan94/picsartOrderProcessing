cfe503b40150c987f1cac37996106b123c3513f7
Annaarushanyan <anna.arushanyan@picsart.com>
2025-06-07 13:01:12 +0400

    Abstraction

---
+++ src/main/java/lessons/Pianist.java
@@ -0,0 +1,14 @@
+package lessons;
+
+public class Pianist extends Musician{
+    @Override
+    public void playMusic() {
+        System.out.println("Playing Piano");
+    }
+
+    @Override
+    public void readNotes(String notes) {
+        System.out.println("reading piano notes");
+
+    }
+}
---
+++ src/main/java/lessons/Programmer.java
@@ -0,0 +1,7 @@
+package lessons;
+
+public interface Programmer {
+    public void writeCode (String requirement);
+    public String languages();
+    public void readCode();
+}
